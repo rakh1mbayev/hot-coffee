e32a52206d07cbb24c4717955f80d98d7a7944a5
Pizza Lover <>
2024-11-04 17:10:10 +0500

    comment

--- internal/service/order_service.go
+++ internal/service/order_service.go
@@ -8,14 +8,14 @@ import (
 )
 
 func PostOrder(order models.Order) {
-	//logic will be here
-	
-	dal.OrderPost(order) //after check go to bucket dal
+	// logic will be here
+
+	dal.OrderPost(order) // after check go to bucket dal
 }
 
 func GetOrder(data []byte, w http.ResponseWriter) {
 	// logic
-	
+
 	w.Header().Set("Content-type", "application/json")
 	w.Write(data)
 }
