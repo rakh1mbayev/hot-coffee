0c5df31f1462214f316846bd4cb0a73518a797f2
Your Name <you@example.com>
2024-11-04 18:43:03 +0500

    small upd

--- internal/handler/menu_handler.go
+++ internal/handler/menu_handler.go
@@ -4,8 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log/slog"
 	"net/http"
+	"bufio"
 	"os"
+	"strings"
 
 	service "hot-coffee/internal/service"
 	model "hot-coffee/models"
@@ -25,13 +28,33 @@ func PostMenu(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetMenuHandler(w http.ResponseWriter, r *http.Request) {
-	file, _ := os.Open(*model.Dir + "/menu.json")
+	file, err := os.Open(*model.Dir + "/menu.json")
 	defer file.Close()
+	if err != nil {
+		slog.Error("Failed to open file "+*model.Dir+" /menu.json", err)
+	}
 	data, _ := io.ReadAll(file)
 	service.GetMenuService(data, w)
 }
 
 func GetMenuID(w http.ResponseWriter, r *http.Request) {
+	file, err := os.Open(*model.Dir + "/menu.json")
+	defer file.Close()
+
+	if err != nil {
+		slog.Error("Failed to open file "+*model.Dir+" /menu.json", err)
+	}
+
+	path := strings.Split(r.URL.Path[1:], "/")
+	id := path[1]
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanWords)
+
+	lastWord := "none"
+	for scanner.Scan() {
+		word := scanner.Text()
+		if word == "\"product_id":""
+	}
 }
 
 func PutMenuID(w http.ResponseWriter, r *http.Request) {
